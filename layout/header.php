31ca0a43254139af5b81d623e37b1c869797cd33
nejlael <elmorabet.n@hotmail.com>
2022-05-28 19:35:37 +0200

    Nettoyage : indentation du code

--- layout/header.php
+++ layout/header.php
@@ -1,55 +1,53 @@
 <?php
-session_start();
-require 'backend/helpers/helpers.php';
-?>
+    session_start();
+    
+    require 'backend/helpers/helpers.php';
+    
+    ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EmeraldBoutique</title>
-    <link rel="stylesheet" href="css/style.css">
-    
-</head>
-<body>
-
-<header>
-        <!-------------------barre d'annonce----------------------->
-        <div class="announcementBar">
-            Bienvenue dans notre e-shop
-        </div>
-        <!-------------------barre de navigation----------------------->
-        <div class="navigation">
-            <a href="index.php" ><img  id="logo" src="./images/icones/logo_emerald.png" alt="logo" height="60px"></a>
-            <img id="menu" src="./images/icones/menu.png" alt="menu" height="25px">
-            <br><br>
-        </div>
-        <!-------------------POP UP MENU----------------------->
-        <div class="popup">
-
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>EmeraldBoutique</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <!-------------------barre d'annonce----------------------->
+            <div class="announcementBar">
+                Bienvenue dans notre e-shop
+            </div>
+            <!-------------------barre de navigation----------------------->
+            <div class="navigation">
+                <a href="index.php" ><img  id="logo" src="./images/icones/logo_emerald.png" alt="logo" height="60px"></a>
+                <img id="menu" src="./images/icones/menu.png" alt="menu" height="25px">
+                <br><br>
+            </div>
+            <!-------------------POP UP MENU----------------------->
+            <div class="popup">
                 <ul id="menu">
-               
-                        <li><a href="catalogue.php">Catalogue</a></li>
-                        <?php if (isLogged()) { ?>
-                            <li><a href="panier.php">Voir mon panier</a></li>
-                            <li><a href="logout.php">Se déconnecter</a></li>
-                        <?php } ?>
-
-                        <?php if(isAdmin()) {?>
-                            <li><a href="categories.php">Les catégories</a></li>
-                            <li><a href="products.php">Les produits</a></li>
-                        <?php } ?>
-                        <?php if (!isLogged()) { ?>
-                        <li><a href="login.php">Connexion/Inscription</a></li>
-                        <?php } ?>
-                               
+                    <li><a href="catalog.php">Catalogue</a></li>
+                    <?php if (isLogged()) { ?>
+                    <li><a href="baskets.php">Voir mon panier</a></li>
+                    <?php } ?>
+                    <?php if(isAdmin()) {?>
+                    <li><a href="categories.php">Les catégories</a></li>
+                    <li><a href="products.php">Les produits</a></li>
+                    <li><a href="users.php">Les utilisateurs</a></li>
+                    <?php } ?>
+                    <?php if (!isLogged()) { ?>
+                    <li><a href="login.php">Connexion/Inscription</a></li>
+                    <?php } ?>
+                    <?php if (isLogged()) { ?>
+                    <li><a href="logout.php">Se déconnecter</a></li>
+                    <?php } ?>
                 </ul>
-
-            <form class="search" action="" mehtod="">
-                <input type="text" name="" id="search">
-                <input type="submit" name="search" value="Rechercher">
-            </form>
-            <br>
-        </div>
-    </header>
+                <form mehtod="POST">
+                    <input type="text" name="" id="search">
+                    <input type="submit" name="search" value="Rechercher">
+                </form>
+                <br>
+            </div>
+        </header>
